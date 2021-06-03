843ad9a97ad4590af23a3a9064a61d3f85c2cec7
Mahir Oberai <mahir.oberai@gmail.com>
2021-06-04 01:50:21 +0800

    auth with spotify api-pulled genres, playlists, tracks, track details

---
+++ my-app/src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '53f10932d2984f3cb7e73be3fdb276ae',
+        ClientSecret: '0be6426e61dc4b1d9999e9c2426424d3'
+    }
+}
+
+export { Credentials };
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import $ from 'jquery';
+import Popper from 'popper.js';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
